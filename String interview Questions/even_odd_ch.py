b6d255549533bdd7189ba04755b12307e0a26bc6
ManuDev03 <manuprajan03@gmail.com>
2021-05-27 12:58:13 +0530

    Characters present in even or odd index

---
+++ String interview Questions/even_odd_ch.py
@@ -0,0 +1,13 @@
+s = input("enter a string:")
+i = 0
+print("characters in even index...")
+while i < len(s):
+    print(s[i])
+    i= i+2
+
+i = 1
+print("characters in odd index")
+while i < len(s):
+    print(s[i])
+    i = i+2
+    
