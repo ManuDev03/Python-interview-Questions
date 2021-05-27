01aeecac02c4d55b0979519a5e4eee2950e4adf3
ManuDev03 <manuprajan03@gmail.com>
2021-05-27 13:17:55 +0530

    Merge 2 strings alternatively

--- String interview Questions/even_odd_ch.py
+++
@@ -1,8 +0,0 @@
-s = input("enter a string:")
-output_even = s[0: :2]
-output_odd = s[1: : 2]
-print("characters in even index...")
-print(output_even)
-print("characters in odd index")
-print(output_odd)
-    
