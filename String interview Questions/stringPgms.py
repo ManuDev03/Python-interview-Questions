f769cd9ccaca135b1a60a6fc8fb9d63900a00e74
ManuDev03 <manuprajan03@gmail.com>
2021-05-27 14:33:30 +0530

    Sorting Alphanumeric inputs to order

--- String interview Questions/stringPgms.py
+++ String interview Questions/stringPgms.py
@@ -1,14 +1,11 @@
-s1 = input("enter first string:")
-s2 = input("enter second string:")
-i = 0
-j = 0
-output = ''
-while i < len(s1) or j < len(s2):
-    if i < len(s1):
-        output = output +s1[i]
-        i = i+1
-    if j < len(s2):
-        output = output +s2[j]
-        j = j+1
-    
+s = input("enter a alphanumeric :")
+alphabets = []
+digits = []
+
+for ch in s :
+    if ch.isalpha():
+        alphabets.append(ch)
+    else :
+        digits.append(ch)
+output = ''.join(sorted(alphabets) + sorted(digits))
 print(output)
